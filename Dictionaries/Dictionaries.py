bc26d9a7d98b2de2bbcdc828643667a079678c02
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-02-02 18:47:51 +0530

    c

---
+++ Dictionaries/Dictionaries.py
@@ -0,0 +1,12 @@
+myDict = {
+    'name': 'Tim',
+    'name2': 'Saksham',
+    'age': 27,
+    'nationality': 'African',
+    'qualification': 'College',
+    'friends': ['a', 'b', 'c']
+}
+
+print(myDict['name'])
+print(len(myDict))
+print(myDict['friends'])
